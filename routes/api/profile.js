3b9f363035147ef5fa19910e91aaf0527a23f68b
James Davey <james.t.davey@gmail.com>
2018-10-16 10:56:09 -0700

    add update profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,10 @@ const Book = require('./../../models/Book');
 const Profile = require('./../../models/Profile');
 const User = require('./../../models/User');
 
+// require validation
+const isEmpty = require('./../../validation/is-empty');
+const validateProfileInput = require('./../../validation/profile');
+
 // @route     /api/profile/test
 // @desc      test profile route
 // @access    public
@@ -58,27 +62,31 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @access    private
 
 router.put('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Profile.find({ handle: req.body.handle })
-    .then(profile => {
-      if (profile && profile.user !== req.user._id)
-        res.status(400).json({ handle: 'This handle has already been taken' });
-      else {
-        const profileFields = { user: req.user._id, date: req.user.date };
-        if (req.body.handle) profileFields.handle = req.body.handle;
-        if (req.body.favoriteBook) profileFields.favoriteBook = req.body.favoriteBook;
-        if (req.body.location) profileFields.location = req.body.location;
-        if (req.body.bio) profileFields.bio = req.body.bio;
-        profileFields.social = {};
-        if (req.body.social.goodreads) profile.social.goodreads = req.body.social.goodreads;
-        if (req.body.social.facebook) profile.social.facebook = req.body.social.facebook;
-        if (req.body.social.instagram) profile.social.instagram = req.body.social.instagram;
+  const errors = validateProfileInput(req.body);
+  if (!isEmpty(errors)) res.status(400).json(errors);
+  else {
+    Profile.find({ handle: req.body.handle || '' })
+      .then(profile => {
+        if (req.body.handle && profile && profile.user !== req.user._id)
+          res.status(400).json({ handle: 'This handle has already been taken' });
+        else {
+          const profileFields = { user: req.user._id, date: req.user.date };
+          if (req.body.handle) profileFields.handle = req.body.handle;
+          if (req.body.favoriteBook) profileFields.favoriteBook = req.body.favoriteBook;
+          if (req.body.location) profileFields.location = req.body.location;
+          if (req.body.bio) profileFields.bio = req.body.bio;
+          profileFields.social = {};
+          if (req.body.goodreads) profile.social.goodreads = req.body.goodreads;
+          if (req.body.facebook) profile.social.facebook = req.body.facebook;
+          if (req.body.instagram) profile.social.instagram = req.body.instagram;
 
-        Profile.findOneAndUpdate({ user: req.user._id }, { $set: profileFields }, { new: true })
-          .then(profile => res.json(profile))
-          .catch(err => res.status(400).json(err));
-      }
-    })
-    .catch(err => res.status(400).json(err));
+          Profile.findOneAndUpdate({ user: req.user._id }, { $set: profileFields }, { new: true })
+            .then(profile => res.json(profile))
+            .catch(err => res.status(400).json(err));
+        }
+      })
+      .catch(err => res.status(400).json(err));
+  }
 });
 
 router.get('/:handle', passport.authenticate('jwt', { session: false }), (req, res) => {
--- validation/profile.js
+++ validation/profile.js
@@ -8,9 +8,44 @@ module.exports = reqBody => {
   reqBody.favoriteBook = !isEmpty(reqBody.favoriteBook) ? reqBody.favoriteBook : '';
   reqBody.location = !isEmpty(reqBody.location) ? reqBody.location : '';
   reqBody.bio = !isEmpty(reqBody.bio) ? reqBody.bio : '';
-  
+  reqBody.goodreads = !isEmpty(reqBody.goodreads) ? reqBody.goodreads : '';
+  reqBody.facebook = !isEmpty(reqBody.facebook) ? reqBody.facebook : '';
+  reqBody.instagram = !isEmpty(reqBody.instagram) ? reqBody.instagram : '';
 
+  if (
+    !Validator.isEmpty(reqBody.handle) &&
+    !Validator.isLength(reqBody.handle, { min: 2, max: 40 })
+  ) {
+    errors.handle = 'Handle must be between 2 and 40 characters';
+  }
 
+  if (
+    !Validator.isEmpty(reqBody.favoriteBook) &&
+    !Validator.isLength(reqBody.favoriteBook, { max: 100 })
+  ) {
+    errors.favoriteBook = 'Favorite Book must be 100 characters or less';
+  }
+
+  if (
+    !Validator.isEmpty(reqBody.location) &&
+    !Validator.isLength(reqBody.location, { min: 2, max: 100 })
+  ) {
+    errors.location = 'Location must be between 2 and 100 characters';
+  }
+
+  if (!Validator.isEmpty(reqBody.bio) && !Validator.isLength(reqBody.bio, { max: 1000 })) {
+    errors.bio = 'Bio must be 1000 characters or less';
+  }
+
+  if (!Validator.isEmpty(reqBody.goodreads) && !Validator.isURL(reqBody.goodreads)) {
+    errors.goodreads = 'Not a valid URL';
+  }
+  if (!Validator.isEmpty(reqBody.facebook) && !Validator.isURL(reqBody.facebook)) {
+    errors.facebook = 'Not a valid URL';
+  }
+  if (!Validator.isEmpty(reqBody.instagram) && !Validator.isURL(reqBody.instagram)) {
+    errors.instagram = 'Not a valid URL';
+  }
 
   return errors;
 };
