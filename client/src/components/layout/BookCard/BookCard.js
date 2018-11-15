64a34e511e08f9175ef51cb3b551d8cce1623387
James Davey <james.t.davey@gmail.com>
2018-11-15 12:05:23 -0800

    fix wrapping issue with book cards as links

--- client/src/components/layout/BookCard/BookCard.js
+++ client/src/components/layout/BookCard/BookCard.js
@@ -18,37 +18,35 @@ class BookCard extends Component {
   };
   render() {
     return (
-      <Link to={`/books/${this.props.book._id}`}>
-        <div
-          className={classes.wrapper}
-          onMouseEnter={this.addHoverHandler}
-          onMouseLeave={this.removeHoverHandler}
-          // onClick={() => this.props.history.push(`/books/${this.props.book._id}`)}
-        >
-          <div>
-            <div
+      <Link
+        to={`/books/${this.props.book._id}`}
+        className={classes.wrapper}
+        onMouseEnter={this.addHoverHandler}
+        onMouseLeave={this.removeHoverHandler}
+      >
+        <div>
+          <div
+            className={[
+              classes['book__cover-top'],
+              this.state.isHoveredOver ? classes['book__cover-top-hover'] : ''
+            ].join(' ')}
+          />
+          <div className={classes['book__cover-title']}>
+            <span
               className={[
-                classes['book__cover-top'],
-                this.state.isHoveredOver ? classes['book__cover-top-hover'] : ''
+                classes['book__cover-title-text'],
+                this.state.isHoveredOver ? classes['book__cover-title-text-hover'] : ''
               ].join(' ')}
-            />
-            <div className={classes['book__cover-title']}>
-              <span
-                className={[
-                  classes['book__cover-title-text'],
-                  this.state.isHoveredOver ? classes['book__cover-title-text-hover'] : ''
-                ].join(' ')}
-              >
-                {this.props.book.title}
-              </span>
-            </div>
-            <div
-              style={{
-                backgroundImage: `url(${this.props.book.image.mediumThumbnail})`
-              }}
-              className={classes['book__cover-image']}
-            />
+            >
+              {this.props.book.title}
+            </span>
           </div>
+          <div
+            style={{
+              backgroundImage: `url(${this.props.book.image.mediumThumbnail})`
+            }}
+            className={classes['book__cover-image']}
+          />
         </div>
       </Link>
     );
