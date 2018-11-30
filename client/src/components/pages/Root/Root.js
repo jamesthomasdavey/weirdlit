8cff3c25d6c787bcaea7821ce2536f8de484e90e
James Davey <james.t.davey@gmail.com>
2018-11-30 13:04:07 -0800

    fix case change error on landing file path

--- client/src/components/pages/Root/Root.js
+++ client/src/components/pages/Root/Root.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Browse from './../Browse/Browse';
-import Landing from './../Landing/Landing';
+import Landing from './../landing/Landing';
 
 class Root extends Component {
   render() {
